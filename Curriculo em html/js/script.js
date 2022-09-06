719726893ca9baa5188cc97f4d0f468850f8f49e
Leoziin <lokinho_ddtank@hotmail.com>
2022-09-06 06:51:37 -0300

    ✨ Meu Curriculo em HTML e uma pagina de compra.

---
+++ Curriculo em html/js/script.js
@@ -0,0 +1,10 @@
+const hamburger = document.querySelector(".hamburger");
+const container = document.querySelector(".container");
+
+
+hamburger.addEventListener("click", () => {
+
+    container.classList.toggle("open");
+
+});
+
